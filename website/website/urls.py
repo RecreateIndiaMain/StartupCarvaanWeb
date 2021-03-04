0307d27225e9372ba7622f7aab9fe7b95480276a
AnubhavGoyal-29 <anubhavgoyal2910@gmail.com>
2021-03-04 13:23:09 +0530

    bla

--- website/website/urls.py
+++ website/website/urls.py
@@ -1,4 +1,4 @@
-"""web URL Configuration
+"""website URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.1/topics/http/urls/
@@ -15,11 +15,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from .views import home
 from django.conf import settings
 from django.conf.urls.static import static
-from general import views as general_views
+
 
 urlpatterns = [
-    path('',general_views.home),
+    path('admin/', admin.site.urls),
+    path('',home),
 ]
 urlpatterns+=static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
