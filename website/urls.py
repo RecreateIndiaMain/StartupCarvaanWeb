504c955d93f3b0947b46a9b25a48935e72efd079
AnubhavGoyal-29 <anubhavgoyal2910@gmail.com>
2021-03-08 22:39:28 +0530

    added

--- website/urls.py
+++ website/urls.py
@@ -22,6 +22,6 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('login/', login),
-    path('',login),
+    path('',home),
 ]
 urlpatterns+=static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
