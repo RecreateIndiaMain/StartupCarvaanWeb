fb5662e1c0c35cb8d6782479a2c24395ce3a6656
AnubhavGoyal-29 <anubhavgoyal2910@gmail.com>
2021-03-09 14:18:16 +0530

    clever

--- website/views.py
+++ website/views.py
@@ -1,5 +1,11 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+import firebase_admin
+from pyrebase import pyrebase
+from firebase_admin import credentials,auth
+cred=credentials.Certificate('serviceAccountKey.json')
+app=firebase_admin.initialize_app(cred)
+print(type(auth))
 def home(request):
     return render(request,'index.html',{})
 
