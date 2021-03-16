042648b860ade3d63881e74c25ece6bac2b4ff48
OmGuptaIND <omgupta0720@gmail.com>
2021-03-16 22:02:11 +0530

    Fix: Joined Pages

--- website/urls.py
+++ website/urls.py
@@ -22,7 +22,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('login/', login, name="login_page"),
-    path('join/', join, name="login_page"),
+    path('join/', join, name="joinus_page"),
     path('achievements/', achievements),
     path('',home),
 ]
