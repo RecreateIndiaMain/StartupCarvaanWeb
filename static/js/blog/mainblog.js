b3b6fd8403dce7b892cf30abcede5e08ee24e895
Yash95Garg <yashagrawal0601@gmail.com>
2021-05-24 22:34:38 +0530

    yash4

---
+++ static/js/blog/mainblog.js
@@ -0,0 +1,12 @@
+function myfunct(){
+    var title = document.getElementById("title");
+    var date = document.getElementById("date");
+    var author = document.getElementById("author");
+    var likes = document.getElementById("likes");
+    var description = document.getElementById("description");
+    document.getElementById("1").innerHTML(title);
+    document.getElementById("2").innerHTML(date);
+    document.getElementById("3").innerHTML(author);
+    document.getElementById("4").innerHTML(likes);
+    document.getElementById("5").innerHTML(description);
+}
