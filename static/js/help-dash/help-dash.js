dba3fa3791b9b53c4249dbf8769e44e037128800
Om Gupta <omgupta0720@gmail.com>
2021-05-29 13:10:47 +0530

    Added: Help Page

--- static/js/help-dash/help-dash.js
+++ static/js/help-dash/help-dash.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const showModal = document.querySelectorAll('#btn-view');
 const dataRow = document.querySelectorAll('.data-row');
 const modalBtn = document.querySelector('.modal-content');
@@ -15,23 +14,12 @@ for(let i = 0; i<dataRow.length; i++)
         var date = document.querySelectorAll("#date")[i];
         var comment = document.querySelectorAll("#comment")[i];
         var status = document.querySelectorAll("#status")[i];
+        var author = document.querySelectorAll("#author")[i];
         modalBtn.classList.remove('hide-modal');
         document.getElementById("1").innerHTML = title.textContent;
         document.getElementById("2").innerHTML = comment.textContent;
         document.getElementById("3").innerHTML = date.textContent;
         document.getElementById("4").innerHTML = status.textContent;
+        document.getElementById("5").innerHTML = author.textContent;
     })
-=======
-function myfunct(){
-    var title = document.getElementById("title").textContent;
-    var date = document.getElementById("date").textContent;
-    var comment = document.getElementById("comment").textContent;
-    var status = document.getElementById("status").textContent;
-    var author = document.getElementById("author").textContent;
-    document.getElementById("1").innerHTML = title;
-    document.getElementById("2").innerHTML = comment;
-    document.getElementById("3").innerHTML = date;
-    document.getElementById("4").innerHTML = status;
-    document.getElementById("5").innerHTML = author;
->>>>>>> f04145b318d6bc2816596527fe7815da61029446
 }
